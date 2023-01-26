d200eb1c682d8db45a4cec19f9cb0550c6d0a5be
Jacobschoenbauer <jhwt65@gmail.com>
2023-01-26 12:06:14 -0600

    moved current code from react-profile

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AllCountainers from "./components/AllContainer";
+import NavTabs from "./components/NavTabs";
+function App() {
+  
+  return (
+    <div>
+      <AllCountainers />
+      <NavTabs />
+    </div>
+  );
+}
+export default App;
