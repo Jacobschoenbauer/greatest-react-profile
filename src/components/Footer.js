ff8a7777085b954332d2beae968f977c80a56680
Jacobschoenbauer <jhwt65@gmail.com>
2023-01-26 21:25:53 -0600

    added links for linkedin facebook github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 export default function Footer() {
-    return (
-      <div>
-       GitHub, Linkedin, Facebook
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <a href="https://github.com/Jacobschoenbauer">GitHub</a>
+
+      <a href="https://www.facebook.com/jacob.schoenbauer">Facebook</a>
+    
+      <a href="https://www.linkedin.com/in/jacob-schoenbauer-281374239/">Linkedin</a>
+    </div>
+  );
+}
