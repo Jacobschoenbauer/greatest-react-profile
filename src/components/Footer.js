2df408efee933b39b9d30829a7ccc0269f213083
Jacobschoenbauer <jhwt65@gmail.com>
2023-01-27 14:19:25 -0600

    footer finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,26 @@
 import React from "react";
+import githubLogo from "./images/Screenshot 2023-01-26 213135.png";
+import facelogo from "./images/facebooklogo.png";
+import linklogo from "./images/linkedlogo.png";
+import "./styles/Footer.css";
 
-export default function Footer() {
+function Footer() {
   return (
     <div>
-      <a href="https://github.com/Jacobschoenbauer">GitHub</a>
+      <p className="footer">
+        <a href="https://github.com/Jacobschoenbauer">
+          <img src={githubLogo} alt="Github"></img>
+        </a>
 
-      <a href="https://www.facebook.com/jacob.schoenbauer">Facebook</a>
-    
-      <a href="https://www.linkedin.com/in/jacob-schoenbauer-281374239/">Linkedin</a>
+        <a href="https://www.facebook.com/jacob.schoenbauer">
+          <img src={facelogo} alt="Github"></img>
+        </a>
+
+        <a href="https://www.linkedin.com/in/jacob-schoenbauer-281374239/">
+          <img src={linklogo} alt="Github"></img>
+        </a>
+      </p>
     </div>
   );
 }
+export default Footer;
