3fb0be1e08c6f9c8147e7001ae365e68e4e22ff2
Jacobschoenbauer <jhwt65@gmail.com>
2023-01-28 13:29:40 -0600

    made downloadedable resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,29 +1,14 @@
 import React from "react";
-const downloadTxtFile = () => {
-  console.log("download logic goes here");
-
-  const file = new Blob({ type: "text/plain" });
-
-  // anchor link
-  const element = document.createElement("a");
-  element.href = URL.createObjectURL(file);
-  element.download = "100ideas-" + Date.now() + ".txt";
-
-  // simulate link click
-  document.body.appendChild(element); // Required for this to work in FireFox
-  element.click();
-};
-
+import resume from "../images/correctresume.png";
+import text from "../images/2023 resume.txt";
 export default function Resume() {
   return (
     <div>
       <h1>Resume</h1>
-
-      <div className="btnDiv">
-        <button id="downloadBtn" onClick={downloadTxtFile} value="download">
-          Download
-        </button>
-      </div>
+      <a href={text} download>
+        <img className="pot" src={resume} alt=""></img>
+      </a>
+      <p>Click Resume to download</p>
     </div>
   );
 }
