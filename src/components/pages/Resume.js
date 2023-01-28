6e3cfeb4df449697da0c145cf878c297a4c5ea41
Jacobschoenbauer <jhwt65@gmail.com>
2023-01-28 10:31:37 -0600

    trtying to setup dowload logic

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React from "react";
+const downloadTxtFile = () => {
+  console.log("download logic goes here");
+
+  const file = new Blob({ type: "text/plain" });
+
+  // anchor link
+  const element = document.createElement("a");
+  element.href = URL.createObjectURL(file);
+  element.download = "100ideas-" + Date.now() + ".txt";
+
+  // simulate link click
+  document.body.appendChild(element); // Required for this to work in FireFox
+  element.click();
+};
 
 export default function Resume() {
   return (
     <div>
       <h1>Resume</h1>
-      <p>
-        Work I like to Work
-      </p>
+
+      <div className="btnDiv">
+        <button id="downloadBtn" onClick={downloadTxtFile} value="download">
+          Download
+        </button>
+      </div>
     </div>
   );
 }
