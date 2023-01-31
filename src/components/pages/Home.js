aa5656d3b3e7f78a5c86db6a7a12f844d5adae61
Jacobschoenbauer <jhwt65@gmail.com>
2023-01-31 12:41:31 -0600

    readme

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 export default function Home() {
   return (
     <div className="mainpages">
       <h1>Home Page</h1>
-      <p>
-        The Starter
+      <p className="coolwrite">
+        Having a growth mindset means believing that a person's abilities aren't
+        innate but can be improved through effort, learning, and persistence. A
+        growth mindset is all about the attitude with which a person faces
+        challenges, how they process failures, and how they adapt and evolve as
+        a result.
       </p>
     </div>
   );
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,6 +3,8 @@ import Screenshot202209 from "../images/Screenshot 2022-09-03 102949.png";
 import Screenshot2022 from "../images/Screenshot 2022-09-25 123904.png";
 import supersearch from "../images/super search photo.png";
 import cooking from "../images/firstpagecooking.png";
+import tech from "../images/bloglogin.png"
+import scheduler from "../images/Screenshot 2022-10-24 102648.png"
 
 export default function Portfolio() {
   return (
@@ -10,19 +12,26 @@ export default function Portfolio() {
       <h1>Portfolio</h1>
       <p className="port">
         <a href="https://jacobschoenbauer.github.io/02-css-portfolio/">
-          <img className="pot" src={Screenshot202209} alt=""></img>
+          <img className="pot" src={Screenshot202209} alt="Basic CSS"></img>
         </a>
 
         <a href="https://jacobschoenbauer.github.io/04-api-jquiz/">
-          <img className="pot" src={Screenshot2022} alt="Github"></img>
+          <img className="pot" src={Screenshot2022} alt="API Quiz"></img>
         </a>
 
         <a href="https://llyruss.github.io/SuperSearch/">
-          <img className="pot" src={supersearch} alt="Github"></img>
+          <img className="pot" src={supersearch} alt="API Search"></img>
         </a>
         <a href="https://whatscookin.herokuapp.com/">
-          <img className="pot" src={cooking} alt="Github"></img>
+          <img className="pot" src={cooking} alt="Recipe Creater"></img>
         </a>
+        <a href="https://working-blog.herokuapp.com/">
+          <img className="pot" src={tech} alt="Github"></img>
+        </a>
+        <a href="https://jacobschoenbauer.github.io/05-scheduler-API/">
+          <img className="pot" src={scheduler} alt="Github"></img>
+        </a>
+      
       </p>
     </div>
   );
